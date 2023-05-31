3fe33e7092bd5ddad4e7652654af92bec3e24a77
GaneshChaturthiCJ <67580297+GaneshChaturthiCJ@users.noreply.github.com>
2023-05-31 11:26:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,58 @@
-import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
 
-const App = () => {
-  // write your code here 
+function CountdownTimer() {
+  const [inputTime, setInputTime] = useState("");
+  const [currentTime, setCurrentTime] = useState(0);
+  let timer = null;
+
+  const startTimer = () => {
+    if (timer !== null) {
+      clearInterval(timer); // Terminate the previous timer if it exists
+    }
+
+    const time = Math.floor(parseFloat(inputTime)); // Take floor of floating input
+    const countdown = time > 0 ? time : 0; // Set initial countdown value
+    setCurrentTime(countdown); // Update initial value
+
+    timer = setInterval(() => {
+      setCurrentTime((prevTime) => {
+        if (prevTime > 0) {
+          return prevTime - 1; // Decrement countdown value
+        } else {
+          clearInterval(timer); // Stop the timer when countdown reaches 0
+          timer = null;
+          return 0;
+        }
+      });
+    }, 1000);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timer !== null) {
+        clearInterval(timer); // Cleanup the timer when component unmounts
+      }
+    };
+  }, []);
 
   return (
-    <div className="wrapper">
-      <div id="whole-center">
-        <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
-        </h1>
+    <div>
+      <div>
+        <input
+          type="number"
+          placeholder="Enter time"
+          value={inputTime}
+          onChange={(e) => setInputTime(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.keyCode === 13) {
+              startTimer();
+            }
+          }}
+        />
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{currentTime}</div>
     </div>
-  )
+  );
 }
 
-
-export default App;
+export default CountdownTimer;
