e290f6b0116b4fc07d78885e0d021cfa1464a3ba
GaneshChaturthiCJ <67580297+GaneshChaturthiCJ@users.noreply.github.com>
2023-05-31 14:43:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-function CountdownTimer() {
-  const [inputTime, setInputTime] = useState("");
-  const [currentTime, setCurrentTime] = useState(0);
-  let timer = null;
+const App = () => {
+  const [count, setCount] = useState(0);
 
-  const startTimer = () => {
-    if (timer !== null) {
-      clearInterval(timer); // Terminate the previous timer if it exists
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      const input = Math.floor(parseFloat(event.target.value));
+
+      if (isNaN(input) || input < 0) {
+        setCount(0);
+      } else {
+        setCount(input);
+      }
     }
-
-    const time = Math.floor(parseFloat(inputTime)); // Take floor of floating input
-    const countdown = time > 0 ? time : 0; // Set initial countdown value
-    setCurrentTime(countdown); // Update initial value
-
-    timer = setInterval(() => {
-      setCurrentTime((prevTime) => {
-        if (prevTime > 0) {
-          return prevTime - 1; // Decrement countdown value
-        } else {
-          clearInterval(timer); // Stop the timer when countdown reaches 0
-          timer = null;
-          return 0;
-        }
-      });
-    }, 1000);
   };
 
   useEffect(() => {
+    let timer;
+
+    if (count > 0) {
+      timer = setInterval(() => {
+        setCount((prevCount) => prevCount - 1);
+      }, 1000);
+    }
+
     return () => {
-      if (timer !== null) {
-        clearInterval(timer); // Cleanup the timer when component unmounts
-      }
+      clearInterval(timer);
     };
-  }, []);
+  }, [count]);
 
   return (
     <div>
-      <div>
-        <input
-          type="number"
-          placeholder="Enter time"
-          value={inputTime}
-          onChange={(e) => setInputTime(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.keyCode === 13) {
-              startTimer();
-            }
-          }}
-        />
-      </div>
-      <div id="current-time">{currentTime}</div>
+      <input id="timeCount" type="number" onKeyDown={handleKeyDown} />
+      <div id="current-time">{count}</div>
     </div>
   );
-}
+};
 
-export default CountdownTimer;
+export default App;
